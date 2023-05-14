1ec100b5ec9a5ed7bf563fdee2859da9c665feaa
M. Arabadzic <manuel.arabadzic@gmail.com>
2023-05-14 14:59:40 +0200

    dependency injection example added

---
+++ poly_farm/Assets/PolyFarm/Scripts/CodeDemonstrations/DependencyInjections/DefaultExample.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.NetworkInformation;
+using UnityEngine;
+
+namespace CodeDemonstrations.DependencyInjections
+{
+    public class MonoMommy : MonoBehaviour
+    {
+        [SerializeField] MonoChick monoChickPrefab;
+
+        void SpawnChick()
+        {
+            var chick = Instantiate(monoChickPrefab);
+            chick.Init(this);
+        }
+    }
+
+    public class MonoChick : MonoBehaviour
+    {
+        MonoMommy mommy;
+
+        public void Init(MonoMommy mom)
+        {
+            mommy = mom;
+        }
+    }
+}
---
+++ poly_farm/Assets/PolyFarm/Scripts/CodeDemonstrations/DependencyInjections/MonoExample.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.NetworkInformation;
+using UnityEngine;
+
+namespace CodeDemonstrations.DependencyInjections
+{
+    public class Mommy
+    {
+        [SerializeField] Chick monoChickPrefab;
+
+        void SpawnChick()
+        {
+            var chick = new Chick(this);
+        }
+    }
+
+    public class Chick
+    {
+        Mommy mommy;
+
+        public Chick(Mommy mom)
+        {
+            mommy = mom;
+        }
+    }
+}
